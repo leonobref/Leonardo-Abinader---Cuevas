050176c34086bc6e7bbf5dfd3394e7d6eb137fa3
leonobref <120611349+leonobref@users.noreply.github.com>
2022-12-15 14:32:34 -0400

    Create 20221215_001.c

---
+++ 20221215_001.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    float num1, num2;
+
+    printf("Digite um numero: ");
+    scanf("%f", &num1);
+    printf("Digite outro numero: ");
+    scanf("%f", &num2);
+
+    printf("\nSoma dos dois numeros = %.1f\n",num1 + num2);
+
+
+    return 0;
+
+}
