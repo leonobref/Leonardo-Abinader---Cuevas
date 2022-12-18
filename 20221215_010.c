bd25e8cc31b9fba9b6978b37e74069e6af2729cc
leonobref <120611349+leonobref@users.noreply.github.com>
2022-12-18 18:19:10 -0400

    Update 20221215_010.c

--- 20221215_010.c
+++ 20221215_010.c
@@ -5,7 +5,7 @@ int main(){
     printf("Digite sua altura(metros): ");
     scanf("%f", &h);
 
-    printf("\nSua altura ideal = %.2f\n", (72.7*h)-58);
+    printf("\nSeu peso ideal = %.2f\n", (72.7*h)-58);
 
     system("pause");
     return 0;
