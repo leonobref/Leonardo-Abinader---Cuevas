3bd8fbab0a491d00f9885c1d5bf634ca6f1a5ae8
leonobref <120611349+leonobref@users.noreply.github.com>
2022-12-21 23:32:03 -0400

    Create 20222220002.c

---
+++ 20222220002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n=0,s=0,c=0;
+    while(c < 50){
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+    if (n % 2 == 1)
+    s = s + n;
+    c = c + 1;
+    }
+    printf("\nSoma = %d", s);
+}
