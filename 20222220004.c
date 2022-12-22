6bc46c210b7ebce8c0dd58387cfe2fef162fb5fb
leonobref <120611349+leonobref@users.noreply.github.com>
2022-12-21 23:56:42 -0400

    Create 20222220004.c

---
+++ 20222220004.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int num,numaior,numenor,i = 1;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    numaior = num;
+    numenor = num;
+
+    while (i < 20){
+        printf("Digite um numero: ");
+        scanf("%d", &num);
+        if(num > numaior)
+        numaior = num;
+        else
+        if(num < numenor)
+        numenor = num;
+        i = i + 1;
+    }
+    printf("\nO menor numero digitado foi: %d", numenor);
+    printf("\nO maior numero digitado foi: %d", numaior);
+}
