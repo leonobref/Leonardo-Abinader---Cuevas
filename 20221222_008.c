a7c11f792f0ae7475e47a6a0f494962778c41a72
leonobref <120611349+leonobref@users.noreply.github.com>
2023-01-11 23:21:12 -0400

    Update 20221222_008.c

--- 20221222_008.c
+++ 20221222_008.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
-
 int main() {
   int n, a, b, h;
-  int sn;
-
   printf("Digite um numero: ");
   scanf("%d" , &n);
 
   for (h = 1; h < n; h++) {
-    sn = 0;
-    for (a = 1; a < h && !sn; a++) {
+    for (a = 1; a < h; a++) {
       b = a;
       while (a*a + b*b < h*h)
-	b += 1;
+	    b += 1;
       if (a*a + b*b == h*h){
 	printf("Inteiro: %d\n", h);
-	sn = 1;
       }
     }
   }
-
   return 0;
 }
