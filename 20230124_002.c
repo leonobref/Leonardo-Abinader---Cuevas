9fcaadf7f453a902c8b98f102195dcd0ff01d394
leonobref <120611349+leonobref@users.noreply.github.com>
2023-01-25 21:12:27 -0400

    Update 20230124_002.c

--- 20230124_002.c
+++ 20230124_002.c
@@ -7,19 +7,28 @@ void imprimirinversa(int v[], int tamanho){
     }
 }
 
-int tamanhovetor(){
-    int a;
-    printf("Digite o tamanho do vetor: ");
-    scanf("%d", &a);
-    return a;
-}
 int main(){
     int i, tamanho;
-    tamanho = tamanhovetor();
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &tamanho);
+    if (0 < tamanho && tamanho <= 100){
     int v[tamanho];
     for (i = 0; i < tamanho; i++){
         printf("Digite o %do numero: ", i+1);
         scanf("%d", &v[i]);
     }
     imprimirinversa(v, tamanho);
+    }
+    else{
+    while (0 >= tamanho || tamanho > 100){
+        printf("Digite o tamanho do vetor (Maximo 100): ");
+        scanf("%d", &tamanho);
+    }
+    int v[tamanho];
+    for (i = 0; i < tamanho; i++){
+    printf("Digite o %do numero: ", i+1);
+    scanf("%d", &v[i]);
+    }
+    imprimirinversa(v, tamanho);
+    }
 }
