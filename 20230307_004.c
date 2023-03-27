ba36288192f6c9d46cad04c2d015f1a8256d6165
leonobref <120611349+leonobref@users.noreply.github.com>
2023-03-27 15:51:26 -0400

    Update 20230307_004.c

--- 20230307_004.c
+++ 20230307_004.c
@@ -7,8 +7,12 @@ int main(){
         printf("Elemento %d do array: ", i+1);
         scanf("%d", (a+i));
     }
+    for (i=0; i<8; i++)
+        printf("\nElemento %d do array eh %d e seu dobro eh %d.",i+1,*(a+i),*(a+i) * 2);
+    printf("\n");  
     for(i=0;i<8;i++){
         if((*a+i)%2 == 0)
             printf("\nElemento %d do array eh %d e seu endereco eh %d.",i+1,*(a+i),a+i);
     }
+    return 0;
 }
